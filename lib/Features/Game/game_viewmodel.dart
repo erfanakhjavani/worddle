ef5e6ac78185168f7bf321c8bf1bde45c45cc094
erfan <ehoseyni1383@gmail.com>
2024-09-26 12:57:02 +0330

    'update'

--- lib/Features/Game/game_viewmodel.dart
+++ lib/Features/Game/game_viewmodel.dart
@@ -6,10 +6,10 @@ import '../../Core/Constants/keyboard.dart';
 import 'game_model.dart';
 
 class GameViewModel extends GetxController with GetTickerProviderStateMixin {
-  //! Wordle game instance
+  //* Wordle game instance
   late WorddleGame game;
 
-  //! Observable variables for game status and UI updates
+  //* Observable variables for game status and UI updates
   var wordMessage = ''.obs;
   var worddleBoard = <List<Letter>>[].obs;
   var currentRow = 0.obs;
@@ -28,13 +28,13 @@ class GameViewModel extends GetxController with GetTickerProviderStateMixin {
   void onInit() {
     super.onInit();
 
-    //! AnimationController for popper effect
+    //TODO AnimationController for popper effect
     popperController = AnimationController(
       vsync: this,
       duration: const Duration(seconds: 2),
     );
 
-    //! Lottie animation controller setup
+    //TODO Lottie animation controller setup
     lottieController = AnimationController(
       vsync: this,
       duration: GetNumUtils(2).seconds,
@@ -49,7 +49,7 @@ class GameViewModel extends GetxController with GetTickerProviderStateMixin {
 
   @override
   void onClose() {
-    //! Dispose of the animation controllers and timer when the controller is closed
+    //TODO Dispose of the animation controllers and timer when the controller is closed
     lottieController.dispose();
     popperController.dispose();
     timer?.cancel();
@@ -81,7 +81,7 @@ class GameViewModel extends GetxController with GetTickerProviderStateMixin {
       helpClickCount.value = 0; //* Reset the help click count
       print('worddle is:  ${game.gameGuess}');
     } catch (e) {
-      //! Error handling in case game initialization fails
+      //* Error handling in case game initialization fails
       wordMessage.value = 'Error starting game: ${e.toString()}';
     }
   }
@@ -134,29 +134,29 @@ class GameViewModel extends GetxController with GetTickerProviderStateMixin {
 
     String guess = game.worddleBoard[currentRow.value].map((e) => e.letter).join(); //* Build the guess from the current row
 
-    //! Check if the guessed word exists
+    //* Check if the guessed word exists
     if (!game.checkWord(guess)) {
       wordMessage.value = 'The word does not exist. Try again.'.tr;
       return;
     }
 
-    //! Flip animation for each letter in the row
+//* Loop through each letter in the guess and animate them
     for (int i = 0; i < game.wordLength; i++) {
-      await animateLetter(i); //* Animate the letter flip
-      checkLetter(i, guess); //* Check if the letter is correct or in the word
+      await animateLetter(i); //* Wait for the animation to complete
+      checkLetter(i, guess); //* Check and change the letter color after the animation
     }
 
     //* Refresh the game board after submitting the guess
     worddleBoard.refresh();
 
-    //! Check if the guess is correct
+    //* Check if the guess is correct
     if (guess == game.gameGuess) {
       wordMessage.value = 'Congratulations 🎉'.tr;
       isGameOver.value = true;
       isCorrectGuess.value = true;
       popperController.forward(from: 0); //* Play the popper animation for winning
     } else if (currentRow.value >= game.maxChances - 1) {
-      //! If out of chances, display the correct word
+      //* If out of chances, display the correct word
       wordMessage.value = 'Game over! Correct word:'.tr;
       wordMessage.value += game.gameGuess;
       isGameOver.value = true;
@@ -171,7 +171,7 @@ class GameViewModel extends GetxController with GetTickerProviderStateMixin {
     game.worddleBoard[currentRow.value][index].code = -1; //* Mark the letter for animation
     worddleBoard.refresh(); //* Refresh the board to show the animation
 
-    //! Delay to allow the flip animation to display
+    //* Delay to allow the flip animation to display
     await Future.delayed(600.ms);
   }
 
@@ -179,18 +179,35 @@ class GameViewModel extends GetxController with GetTickerProviderStateMixin {
   void checkLetter(int index, String guess) {
     String char = guess[index];
 
-    //! Check if the letter is in the correct position or exists in the word
-    if (game.gameGuess.contains(char)) {
-      if (game.gameGuess[index] == char) {
-        game.worddleBoard[currentRow.value][index].code = 1; //* Correct position
-        letterColors[char] = Colors.green;
-      } else {
-        game.worddleBoard[currentRow.value][index].code = 2; //* Correct letter, wrong position
-        letterColors[char] = Colors.amber.shade400;
+    // Create a Map to count occurrences of each character in the correct word
+    Map<String, int> charCount = {};
+    for (var letter in game.gameGuess.split('')) {
+      charCount[letter] = (charCount[letter] ?? 0) + 1;
+    }
+
+    // First pass: check letters that are in the correct position
+    for (int i = 0; i <= index; i++) {
+      String currentChar = guess[i];
+      if (game.gameGuess[i] == currentChar) {
+        game.worddleBoard[currentRow.value][i].code = 1; // Correct position (green)
+        letterColors[currentChar] = Colors.green;
+        charCount[currentChar] = charCount[currentChar]! - 1; // Decrease count for correct position
       }
-    } else {
-      game.worddleBoard[currentRow.value][index].code = 3; //* Incorrect letter
-      letterColors[char] = Colors.grey.shade700;
+    }
+
+    //* Second pass: check letters that are in the word but in the wrong position
+    for (int i = 0; i <= index; i++) {
+      String currentChar = guess[i];
+      if (game.worddleBoard[currentRow.value][i].code != 1) { //* Skip if already green
+        if (game.gameGuess.contains(currentChar) && charCount[currentChar]! > 0) {
+          game.worddleBoard[currentRow.value][i].code = 2; //* Wrong position (yellow)
+          letterColors[currentChar] = Colors.amber.shade400;
+          charCount[currentChar] = charCount[currentChar]! - 1; //* Decrease count for wrong position
+        } else {
+          game.worddleBoard[currentRow.value][i].code = 3; //* Incorrect letter (grey)
+          letterColors[currentChar] == Colors.green ? null : letterColors[currentChar] = Colors.grey.shade700;
+        }
+      }
     }
   }
 
@@ -201,33 +218,33 @@ class GameViewModel extends GetxController with GetTickerProviderStateMixin {
       return;
     }
 
-    //! Collect all keys from the keyboard
+    //* Collect all keys from the keyboard
     List<String> allKeys = isFarsi.value
         ? [...row1Farsi, ...row2Farsi, ...row3Farsi]
         : [...row1English, ...row2English, ...row3English];
 
-    //! Remove keys from the target word and special keys like DEL and DO
+    //* Remove keys from the target word and special keys like DEL and DO
     String targetWord = game.gameGuess;
     allKeys.removeWhere((key) => targetWord.contains(key) || key == 'DEL' || key == 'DO');
 
     helpClickCount.value++; // Increment help click count
 
-    //! Calculate how many keys to disable based on the click count
+    //* Calculate how many keys to disable based on the click count
     int disableCount = ((allKeys.length / 6) * helpClickCount.value).round();
 
     //* Ensure we don't disable more keys than available
     disableCount = disableCount > allKeys.length ? allKeys.length : disableCount;
 
-    //! Shuffle the keys and pick random ones to disable
+    //* Shuffle the keys and pick random ones to disable
     allKeys.shuffle();
     List<String> disabledKeys = allKeys.take(disableCount).toList();
 
-    //! Disable the selected keys by changing their color
+    //* Disable the selected keys by changing their color
     for (String key in disabledKeys) {
       letterColors[key] = Colors.grey.shade500;
     }
 
-    //! Refresh the keyboard to reflect the changes
+    //* Refresh the keyboard to reflect the changes
     worddleBoard.refresh();
   }
 
